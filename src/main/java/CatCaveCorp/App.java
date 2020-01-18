68f184b3faf53bbf8e2fd23debf4db8778ce1323
Stah <stahthecat@gmail.com>
2020-01-18 14:28:19 +0100

    nowa pętla za zdrówko GITa

--- src/main/java/CatCaveCorp/App.java
+++ src/main/java/CatCaveCorp/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello JAVA EDYCJA!!!!!" );
 
         for(int i = 0; i < 10; i++){
-            System.out.println("JESTEM MISTRZEM GITOWANIA");
+            System.out.println(i +" kieliszek za Gita");
         }
 
 
