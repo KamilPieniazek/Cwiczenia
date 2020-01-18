3ac090036bc42372054aad1df7ed0e6c8c36eb52
Lukasz <krzyzak90@gmail.com>
2020-01-18 15:01:45 +0100

    'staszek'

--- src/main/java/CatCaveCorp/App.java
+++ src/main/java/CatCaveCorp/App.java
@@ -1,19 +1,16 @@
 package CatCaveCorp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello JAVA EDYCJA!!!!!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA EDYCJA!!!!!");
 
-        for(int i = 0; i < 10; i++){
-            System.out.println(i +" kieliszek za Gita");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " kieliszek za Gita");
         }
 
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " lampka z wina");
+        }
     }
+
 }
