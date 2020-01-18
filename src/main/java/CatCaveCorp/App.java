e5f94a4d51205cf3c8425b81e527d190d5e9a99a
Michal-dotcom <michalwaszkiewicz1996@gmail.com>
2020-01-18 14:42:45 +0100

    Update App.java

--- src/main/java/CatCaveCorp/App.java
+++ src/main/java/CatCaveCorp/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello JAVA EDYCJA!!!!!" );
+        System.out.println( "Hello java edycja!!!! O TU DOPISAŁEM" );
 
         for(int i = 0; i < 10; i++){
             System.out.println(i +" kieliszek za Gita");
