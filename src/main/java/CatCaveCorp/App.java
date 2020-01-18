078000f0cf657d8e128e7f413d66f526eef34e1c
Stah <stahthecat@gmail.com>
2020-01-18 14:02:50 +0100

    dodałem petlte zachwalającą samego siebie

--- src/main/java/CatCaveCorp/App.java
+++ src/main/java/CatCaveCorp/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello JAVA EDYCJA!!!!!" );
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("JESTEM MISTRZEM GITOWANIA");
+        }
+
+
     }
 }
